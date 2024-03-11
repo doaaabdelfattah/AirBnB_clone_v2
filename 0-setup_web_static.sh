fcc11dd3db49400bc20d54235a52f308bc91c4a3
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-11 03:24:30 +0200

    “task0”

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
+# Install Nginx
+apt-get update
+apt-get install -y nginx
+
+# Create directorirs & files
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+# Create the symbolic link
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Change Permissions
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+# NGINX Config File
+printf %s "server {
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}
+
+" > /etc/nginx/sites-available/default
+
+
+# Restart server
+service nginx restart
