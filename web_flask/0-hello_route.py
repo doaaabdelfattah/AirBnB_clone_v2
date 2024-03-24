1e8aea96b15ab79fe2acc0cf8595764f6286b0f5
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-24 11:55:11 +0200

    “upload”

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+'''
+Flask Module
+'''
+from flask import Flask
+
+
+web_app = Flask(__name__)
+
+@web_app.route("/", strict_slashes=False)
+def homepage():
+    ''' return home message'''
+    return "Hello HBNB!"
+
+
+
+if __name__ == "__main__":
+    web_app.run(debug=True)
