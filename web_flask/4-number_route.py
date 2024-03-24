f5564ecd5c7c40cd1689ca5d6202e62f539a439e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-24 12:24:57 +0200

    “task4”

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''
+Flask Module
+'''
+from flask import Flask, abort
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def homepage():
+    ''' return home message'''
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' return home message'''
+    return "HBNB"
+
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    ''' return text '''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@web_app.route("/python", strict_slashes=False)
+@web_app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    ''' return text '''
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@web_app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    ''' return text '''
+    # Check if s is an integer
+    if isinstance(n, int):
+        return"{} is a number".format(n)
+    else:
+        abort(404)
+
+
+if __name__ == "__main__":
+    web_app.run(debug=True, host="0.0.0.0")
