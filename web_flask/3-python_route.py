ef39269f6e01b61f7db36fbe574feea951e4787d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-24 12:15:55 +0200

    “task3”

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+Flask Module
+'''
+from flask import Flask
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def homepage():
+    ''' return home message'''
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' return home message'''
+    return "HBNB"
+
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    ''' return text '''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@web_app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    ''' return text '''
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    web_app.run(debug=True, host="0.0.0.0")
