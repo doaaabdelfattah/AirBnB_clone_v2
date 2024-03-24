6791b2f277e4d0569c04a698d917a796896320e1
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-24 12:08:29 +0200

    “upload”

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Flask Module
+'''
+from flask import Flask
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def homepage():
+    ''' return home message'''
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' return home message'''
+    return "HBNB"
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    ''' return text '''
+    return "C is %s" % '_'.join(text.split('_'))
+
+
+if __name__ == "__main__":
+    web_app.run(debug=True, host="0.0.0.0")
