d838d5e1e4b307cccdb83a6aeedc1d8147ad326d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-25 02:07:16 +0200

    “upload”

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
 from models.engine.file_storage import FileStorage
+import models
 
 class State(BaseModel, Base):
     """ State class """
@@ -21,6 +22,5 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """ Getter """
-            storage = FileStorage()
-            cities_dict = storage.all(City)
+            cities_dict = models.storage.all(City)
             return [city for city in cities_dict.values() if city.state_id == self.id]
