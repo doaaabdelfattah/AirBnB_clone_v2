154136e9be389a9d3e21c84b42961df58a3543a5
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-25 02:52:50 +0200

    “task8”

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Flask Module
+'''
+from flask import Flask, render_template
+from models import storage
+
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/states_list", strict_slashes=False)
+def state_list():
+    ''' return list of states'''
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
+
+
+@web_app.route("/cities_by_states", strict_slashes=False)
+def state_list():
+    ''' return list of states'''
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
+
+
+@web_app.teardown_appcontext
+def teardown(exc):
+    '''when the application context is torn down
+    '''
+    storage.close()
+
+
+if __name__ == "__main__":
+    web_app.run(debug=True, host="0.0.0.0")
