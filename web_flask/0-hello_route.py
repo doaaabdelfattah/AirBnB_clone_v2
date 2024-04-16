4f0969dedabddd0db2988b26a0db88df142a6d65
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-04-16 04:48:04 +0200

    “task0”

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,14 @@ Flask Module
 '''
 from flask import Flask
 
-web_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@web_app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def homepage():
     ''' return home message'''
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    web_app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
