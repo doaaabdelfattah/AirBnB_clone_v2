b8f3374e26fb65eaa8702904d014bd826b5c8a0c
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-04-18 23:31:27 +0200

    “task3”

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,54 +4,54 @@ Flask Module
 '''
 from flask import Flask, render_template
 
-web_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@web_app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def homepage():
     ''' return home message'''
     return "Hello HBNB!"
 
 
-@web_app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     ''' return home message'''
     return "HBNB"
 
 
-@web_app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_fun(text):
     ''' return text '''
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@web_app.route("/python", strict_slashes=False)
-@web_app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_text(text='is cool'):
     ''' return text '''
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
-@web_app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     ''' return number '''
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
-@web_app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     ''' return html page '''
     return render_template("5-number.html", n=n)
 
 
-@web_app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_even(n):
     ''' return html page '''
     return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
-    web_app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
